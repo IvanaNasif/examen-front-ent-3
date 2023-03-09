45a684cf3b6d53b9893b9b116307c07366aae25b
IvanaNasif <ivananasif@gmail.com>
2023-03-08 22:17:38 -0300

    parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import { useState } from 'react'
-import './App.css'
+import Header from'./components/Header'
+import Formulario from './components/Formulario'
+import Pedido from './components/Pedido'
 
+
+const pedidos =[
+  {id:1, name: "remera"},
+  {id:2, name: "pantalon"},
+  {id:3, name: "zapatillas"},
+  {id:4, name: "gorras"},
+]
+
+let id=0 
 
 function App() {
-  
+  const [pedidos, setPedidos] = useState([])
+
+function handleAddPedido(pedido){
+  console.log(pedido);
+  const newPedidos =[...pedidos]
+  newPedidos.push({
+    id: id ++, 
+    name:pedido,
+  })
+  console.log(newPedidos)
+  setPedidos (newPedidos)
+}
+function handleDeletePedido(id){
+  const newPedidos= pedidos.filter((pedido) => pedido.id !== id)
+  setPedidos (newPedidos)
+}
 
   return (
     <div className="App">
-     
+      <Header title="Tienda de Ropa" />
+      <Formulario onSubmit ={handleAddPedido}/>
+      <hr />
+      {pedidos.map((pedido) =>{
+      return 
+      <Pedido  key ={pedido.id} pedido={pedido} onDelete={handleDeletePedido}/>  
+})}
     </div>
   )
 }
